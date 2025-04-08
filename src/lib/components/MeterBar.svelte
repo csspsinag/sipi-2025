132973f8ff8b542acaf32c60781182416e64b047
vaynealtapascine <vaynealtapascine@proton.me>
2025-04-08 19:53:57 +0800

    add: Create meter bar + linked indicator.

---
+++ src/lib/components/MeterBar.svelte
@@ -0,0 +1,174 @@
+<script lang="ts">
+	import NumberFlow, { NumberFlowGroup } from '@number-flow/svelte';
+	import { Meter, useId } from 'bits-ui';
+	import type { ComponentProps } from 'svelte';
+	import { onMount } from 'svelte';
+
+	let {
+		max = 100,
+		value = 0,
+		min = 0,
+		count = 0,
+		countMax = 0,
+		label,
+		valueLabel
+	}: ComponentProps<typeof Meter.Root> & {
+		count: number;
+		countMax: number;
+		label: string;
+		valueLabel: string;
+	} = $props();
+
+	const labelId = useId();
+
+	let mainTurnoutLabel = $state({ number: 0.0 });
+	let mainTurnoutCount = $state({ number: 0.0 });
+	let mainTurnoutCountMax = $state({ number: 0.0 });
+	function rollUp(stateVar: { number: number }, value: number) {
+		stateVar.number = value;
+	}
+
+	onMount(() => {
+		setInterval(() => {
+			rollUp(mainTurnoutLabel, value / 100);
+			rollUp(mainTurnoutCount, count);
+			rollUp(mainTurnoutCountMax, countMax);
+		}, 1000);
+	});
+</script>
+
+<div class="shared-width">
+	<Meter.Root aria-labelledby={labelId} aria-valuetext={valueLabel} {value} {min} {max}>
+		<div
+			data-meter-bar
+			style="transform: translateX(calc(-{(1 - mainTurnoutLabel.number) * 100}%))"
+		></div></Meter.Root
+	>
+	<div class="container-lock">
+		<div
+			class="pointer-arrow"
+			style="transform: translateX(calc((-50cqw) + {mainTurnoutLabel.number * 100}cqw))"
+		></div>
+
+		<div
+			class="percentage-map"
+			style="transform: translateX(calc((-50cqw) + {mainTurnoutLabel.number * 100}cqw))"
+		>
+			<NumberFlowGroup>
+				<p>{label}</p>
+				<div class="main-turnout-percent">
+					<NumberFlow
+						value={mainTurnoutLabel.number}
+						format={{ style: 'percent', minimumFractionDigits: 2 }}
+					/>
+				</div>
+				<div class="main-turnout-count">
+					<NumberFlow value={mainTurnoutCount.number} format={'0'} />/<NumberFlow
+						value={mainTurnoutCountMax.number}
+						format={'0'}
+					/>
+				</div>
+			</NumberFlowGroup>
+		</div>
+	</div>
+</div>
+
+<style>
+	.shared-width {
+		container-type: inline-size;
+		width: 100%;
+	}
+
+	:global([data-meter-root]) {
+		--meter-bar-color: #e63386;
+		background-color: #eec6d8;
+		box-shadow: 0 0.25rem 1rem rgba(122, 122, 122, 0.596);
+
+		overflow: hidden;
+
+		height: 20px;
+		width: 100%;
+	}
+
+	:global([data-meter-bar]) {
+		height: 100%;
+		width: 100%;
+		background: linear-gradient(
+			to right,
+			color-mix(in oklab, var(--meter-bar-color), #000) 0%,
+			var(--meter-bar-color) 100%
+		);
+	}
+
+	.container-lock {
+		container-type: inline-size;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	:is(.percentage-map) {
+		display: flex;
+		flex-direction: column;
+		align-items: end;
+		justify-content: center;
+
+		transform-origin: 100% 0;
+
+		width: max-content;
+		padding: clamp(0.25rem, 2vw, 0.5rem) clamp(0.125rem, 1vw, 0.75rem) clamp(0.25rem, 2vw, 0.5rem)
+			clamp(0.25rem, 2vw, 1rem);
+		margin: 0.25rem 0 0;
+
+		font-variant-numeric: tabular-nums;
+		font-family: 'Hanken Grotesk', sans-serif;
+		font-size: clamp(0.5rem, 3cqw, 2rem);
+		font-weight: 500;
+
+		color: #ffe8a3;
+		background: linear-gradient(to right, #880039, #a81459);
+		border-radius: 1rem 1rem 1rem 1rem;
+		box-shadow: 0 0.25rem 1rem rgba(122, 122, 122, 0.596);
+
+		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.3s;
+
+		.pointer-arrow {
+			position: absolute;
+			width: 5px;
+			height: 5px;
+			border-left: 0.5rem solid transparent;
+			border-right: 0.5rem solid transparent;
+			border-bottom: 0.5rem solid #ffe8a3;
+			transform: translateX(-50%) translateY(-50%);
+		}
+
+		p {
+			margin: 0;
+			padding: 0;
+			font-size: clamp(0.5rem, 5cqw, 2rem);
+			font-weight: 600;
+		}
+	}
+
+	.main-turnout-percent {
+		font-family: 'Bricolage Grotesque', sans-serif !important;
+		font-size: clamp(1rem, 8cqw, 4rem);
+		font-weight: 700;
+		color: #fff7fb;
+		margin: calc(clamp(-1.25rem, 2vw, 1rem) * -1) 0 calc(clamp(-1.25rem, 1.75vw, 1rem) * -1);
+	}
+
+	.main-turnout-count {
+		display: inline-flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+
+		font-family: 'Bricolage Grotesque', sans-serif !important;
+		font-size: clamp(0.5rem, 2cqw, 2rem);
+		font-weight: 500;
+		color: #b2f3f3;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,20 +13,376 @@
 	import timezone from 'dayjs/plugin/timezone.js';
 	import BackToTop from '$lib/components/BackToTop.svelte';
 	import SinagBar from '$lib/components/SinagBar.svelte';
+	import { Meter } from 'bits-ui';
+	import MeterBar from '$lib/components/MeterBar.svelte';
 	dayjs.extend(utc);
 	dayjs.extend(timezone);
+
+	const SECOND = 1000;
+
+	async function fetchTurnoutData(): Promise<{
+		collegewideTurnoutCount: number;
+		collegewideTurnoutCountMax: number;
+		programTurnout: {
+			program: string;
+			type: string;
+			turnoutCount: number;
+			turnoutCountMax: number;
+		}[];
+	}> {
+		return fetch('turnout.json')
+			.then((response) => response.json())
+			.then((data) => {
+				return data;
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+	let turnoutData = $state({
+		collegewideTurnoutCount: 0,
+		collegewideTurnoutCountMax: 1,
+		programTurnout: [
+			{
+				program: 'Anthropology',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Anthropology',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Geografia',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Geografia',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Kasaysayan',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Kasaysayan',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Linguistics',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Linguistics',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Philosophy',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Philosophy',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Political Science',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Political Science',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Psychology',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Psychology',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Sociology',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Sociology',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Non-Major',
+				type: 'Undergraduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			},
+			{
+				program: 'Population Institute',
+				type: 'Graduate',
+				turnoutCount: 0,
+				turnoutCountMax: 2628
+			}
+		]
+	});
+	let turnoutPlaceholder = $state(() => {
+		return {
+			collegewide: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'CSSP turnout'
+			},
+			anthropology: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Anthropology'
+			},
+			geografia: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Geografia'
+			},
+			kasaysayan: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Kasaysayan'
+			},
+			linguistics: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Linguistics'
+			},
+			philosophy: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Philosophy'
+			},
+			politicalScience: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Political Science'
+			},
+			psychology: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Psychology'
+			},
+			sociology: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Sociology'
+			},
+			nonMajor: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Non-Major'
+			},
+			populationInstitute: {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Population Institute'
+			}
+		};
+	});
 
 	let timeAnchor = $state(dayjs());
 	setContext('timeZoneGuess', dayjs.tz.guess());
 	setContext('timeAnchor', () => timeAnchor);
 
+	function turnoutBreakdownLookup(program: string): any {
+		let returnObject = {
+			value: 0,
+			max: 2628,
+			valueLabel: '0',
+			count: 0,
+			countMax: 2628,
+			label: ''
+		};
+
+		returnObject.count = turnoutData.programTurnout
+			.filter((turnout) => turnout.program === program)
+			.flatMap((turnout) => turnout.turnoutCount)
+			.reduce((a, b) => a + b);
+		returnObject.countMax = turnoutData.programTurnout
+			.filter((turnout) => turnout.program === program)
+			.flatMap((turnout) => turnout.turnoutCountMax)
+			.reduce((a, b) => a + b);
+		returnObject.value = (returnObject.count / returnObject.countMax) * 100;
+		returnObject.max = returnObject.countMax;
+		returnObject.valueLabel = `${returnObject.value} / ${returnObject.max}`;
+		returnObject.label = program;
+
+		return returnObject;
+	}
+
+	turnoutPlaceholder = () => {
+		return {
+			collegewide: {
+				value: (turnoutData.collegewideTurnoutCount / turnoutData.collegewideTurnoutCountMax) * 100,
+				max: turnoutData.collegewideTurnoutCountMax,
+				valueLabel: `${turnoutData.collegewideTurnoutCount} / ${turnoutData.collegewideTurnoutCountMax}`,
+				count: turnoutData.collegewideTurnoutCount,
+				countMax: turnoutData.collegewideTurnoutCountMax,
+				label: 'CSSP turnout'
+			},
+			anthropology: turnoutBreakdownLookup('Anthropology') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Anthropology'
+			},
+			geografia: turnoutBreakdownLookup('Geografia') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Geografia'
+			},
+			kasaysayan: turnoutBreakdownLookup('Kasaysayan') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Kasaysayan'
+			},
+			linguistics: turnoutBreakdownLookup('Linguistics') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Linguistics'
+			},
+			philosophy: turnoutBreakdownLookup('Philosophy') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Philosophy'
+			},
+			politicalScience: turnoutBreakdownLookup('Political Science') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Political Science'
+			},
+			psychology: turnoutBreakdownLookup('Psychology') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1
+			},
+			sociology: turnoutBreakdownLookup('Sociology') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Sociology'
+			},
+			nonMajor: turnoutBreakdownLookup('Non-Major') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Non-Major'
+			},
+			populationInstitute: turnoutBreakdownLookup('Population Institute') ?? {
+				value: 0,
+				max: 1,
+				valueLabel: '0',
+				count: 0,
+				countMax: 1,
+				label: 'Population Institute'
+			}
+		};
+	};
+
 	onMount(() => {
 		const interval = setInterval(() => {
 			timeAnchor = dayjs();
-		}, 1000);
+			console.log('Checking... (every second)');
+		}, 1 * SECOND);
+
+		const turnoutCheck = setInterval(async () => {
+			const fetch = await fetchTurnoutData();
+			turnoutData = fetch;
+			console.log('Checking... (every minute)');
+		}, 5 * SECOND);
 
 		return () => {
 			clearInterval(interval);
+			clearInterval(turnoutCheck);
 		};
 	});
 </script>
@@ -58,13 +414,30 @@
 	<meta name="twitter:image" content="https://sinag.press/sipi/2025/sipi-wide.png" />
 </svelte:head>
 
-<SinagBar>SIPI 2025 Election Countdown</SinagBar>
+<SinagBar>SIPI 2025 Turnout Tracker</SinagBar>
 <div class="main-page">
 	<div class="central-timer">
 		<img src={sipiLogo} id="sipiLogo" />
+
+		<div class="bar-clamp">
+			<MeterBar {...turnoutPlaceholder().collegewide} />
+		</div>
 		<h2>Time Until CSSP Election End</h2>
 		<Timer date={electionDatesLookup.get('updElectionEnd')!.date} />
 	</div>
+</div>
+
+<div class="under-fold">
+	<MeterBar {...turnoutPlaceholder().anthropology} />
+	<MeterBar {...turnoutPlaceholder().geografia} />
+	<MeterBar {...turnoutPlaceholder().kasaysayan} />
+	<MeterBar {...turnoutPlaceholder().linguistics} />
+	<MeterBar {...turnoutPlaceholder().philosophy} />
+	<MeterBar {...turnoutPlaceholder().politicalScience} />
+	<MeterBar {...turnoutPlaceholder().psychology} />
+	<MeterBar {...turnoutPlaceholder().sociology} />
+	<MeterBar {...turnoutPlaceholder().nonMajor} />
+	<MeterBar {...turnoutPlaceholder().populationInstitute} />
 </div>
 <BackToTop />
 
@@ -118,21 +491,41 @@
 		font-variant-numeric: tabular-nums;
 		min-width: 75vw;
 
+		.bar-clamp {
+			width: clamp(90vw, 800px, 60vw);
+		}
+
 		h2 {
-			margin-bottom: clamp(0, 4vw, 3rem);
+			margin-top: clamp(-0.5rem, 4vw, 4rem);
+			margin-bottom: clamp(-0.5rem, 4vw, 2rem);
 			z-index: 1;
 		}
 
-		@media (max-width: 800px) {
+		@media (max-width: 1500px) {
 			h2 {
-				margin-bottom: -1em;
+				margin-bottom: 0em;
+			}
+		}
+
+		@media (max-width: 1000px) {
+			h2 {
+				margin-bottom: -2em;
 			}
 		}
 	}
 
+	.under-fold {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		margin-top: clamp(-0.5rem, 4vw, 4rem);
+	}
+
 	#sipiLogo {
 		width: 25vw;
-		margin: -5rem 0 -2rem 0;
+		margin: -5rem 0 calc(clamp(-0.5rem, 4vw, 4rem) * -1) 0;
 		z-index: 2;
 	}
 
